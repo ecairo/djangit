58d7e51ca681a147e4a2eb825423ff0d920170f1
Ernesto Perez Cairo <ecairo@abalt.org>
2014-09-07 16:50:03 -0700

    Adding Repo model

---
+++ apps/git_a/migrations/0003_auto__add_repo.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Repo'
+        db.create_table(u'git_a_repo', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('path', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('public', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal(u'git_a', ['Repo'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Repo'
+        db.delete_table(u'git_a_repo')
+
+
+    models = {
+        u'git_a.repo': {
+            'Meta': {'object_name': 'Repo'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'git_a.reposroot': {
+            'Meta': {'object_name': 'ReposRoot'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['git_a']
--- apps/git_a/models.py
+++ apps/git_a/models.py
@@ -1,6 +1,9 @@
 import os
 
 from django.db import models
+from django.utils.translation import ugettext as _
+
+from .utils import get_repo
 
 
 class ReposRoot(models.Model):
@@ -16,3 +19,21 @@ class ReposRoot(models.Model):
 
     def __unicode__(self):
         return u'%s' % self.path
+
+
+class Repo(models.Model):
+    path = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=100, verbose_name=_(u'Repo Name'))
+    description = models.TextField(verbose_name=_('Description'),
+                                   blank=True, null=True)
+    public = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _('Repo')
+        verbose_name_plural = _('Repos')
+
+    def __unicode__(self):
+        return u'%s' % self.name
+
+    def repo(self):
+        return get_repo(self.path)
